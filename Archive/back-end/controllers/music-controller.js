97e60a7fa3f389873bb261245de1c454595338b3
Arun Kumar <java.kumar.arun@gmail.com>
2025-07-24 12:48:57 +0530

    update

--- Archive/back-end/controllers/music-controller.js
+++ Archive/back-end/controllers/music-controller.js
@@ -1,10 +1,25 @@
+import Playlist from "./../api/v2/models/playlist.js"
+import Song from "./../api/v2/models/song.js"
+
 export const getAllSongs = (req, res)=>{
     res.json({message:'Get All Songs '});
 }
 export const searchSongs = (req, res)=>{
     res.json({message:'Search Songs '});
 }
-export const addSong = (req, res)=>{
+export const addSong = async (req, res)=>{
+    // req.body.name
+    // req.body.playlistId
+
+    // save song in database
+    const song = new Song({name: req.body.song_name})
+    await song.save();
+    // find playlist from database
+    const palylist = await Playlist.findById({_id: req.body.playlistId});
+    palylist.songs.push(song._id);
+    // put saved song id into playlist
+    await palylist.save();
+
     res.json({message:'Add Song '});
 }
 
